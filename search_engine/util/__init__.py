1fe216e40cf772a41efae17df857e5360af3f0c7
kalaww <ceddesgranges@gmail.com>
2017-02-22 14:40:21 +0100

    add wiki_dump_collector

--- search_engine/util/__init__.py
+++ search_engine/util/__init__.py
@@ -1,4 +1,7 @@
 import unidecode
+import itertools
+import sys
+import time
 
 def process_word(word):
     """
@@ -8,10 +11,80 @@ def process_word(word):
     """
     return unidecode.unidecode(word).lower()
 
-def normalize_page_content(content):
-    bad_chars = ['"', '\'', '.', ',', '{', '}', '=', '*', '[', ']', '|', '@', ':',
+def normalize_text(content):
+    """
+    Remove unused charactere and multiple spaces form a text
+    :param content:
+    :return:
+    """
+    bad_chars = ['"', '.', ',', '{', '}', '=', '*', '[', ']', '|', '@', ':',
                  '!', '?', '%', '$', '&amp;', '&quot;', '~', '/']
     for bad in bad_chars:
         content = content.replace(bad, ' ')
     content = ' '.join(content.split())
     return content.strip()
+
+def count_lines_in_file(filename):
+    """
+    Fast line count of a file
+    :param filename:
+    :return:
+    """
+    f = open(filename, 'r')
+    bufgen = itertools.takewhile(lambda x: x, (f.read(1024*1024) for _ in itertools.repeat(None)))
+    return sum( buf.count(b'\n') for buf in bufgen if buf )
+
+
+def pretty_number(n):
+    """
+    String representation with space each 3 characteres
+    :param n: number
+    :return: pretty string representation
+    """
+    a = str(n)[::-1]
+    return (' '.join([a[i:i + 3] for i in range(0, len(a), 3)]))[::-1]
+
+
+class ProgressBar:
+
+    def __init__(self, total):
+        self.total = total
+        self.last_print = ''
+        self.start_time = time.time()
+
+    def reset_time(self):
+        self.start_time = time.time()
+
+    def print_progress(self, value):
+        time_diff = time.time() - self.start_time
+        hours, rem = divmod(time_diff, 3600)
+        minutes, seconds = divmod(rem, 60)
+        if hours > 0 :
+            t = "{:0>2}h {:0>2}m {:0>2}s".format(int(hours),int(minutes),int(seconds))
+        else :
+            t = "{:0>2}m {:0>2}s".format(int(minutes),int(seconds))
+
+        if value == 0 :
+            eta = '?'
+        else:
+            remaining_time = self.total * time_diff / value
+            remaining_time -= time_diff
+            hours, rem = divmod(remaining_time, 3600)
+            minutes, seconds = divmod(rem, 60)
+            if hours > 0:
+                eta = "{:0>2}h {:0>2}m {:0>2}s".format(int(hours), int(minutes), int(seconds))
+            else:
+                eta = "{:0>2}m {:0>2}s".format(int(minutes), int(seconds))
+
+        s = '[{percent}%] {time} - {current} / {total} - ETA: {eta} '.format(
+            percent=int(100 * value / self.total),
+            current=pretty_number(value),
+            total=pretty_number(self.total),
+            time=t,
+            eta=eta
+        )
+        sys.stdout.write('\b' * len(self.last_print))
+        sys.stdout.write('\r')
+        sys.stdout.write(s)
+        sys.stdout.flush()
+        self.last_print = s
---
+++ search_engine/wiki_dump_collector.py
@@ -0,0 +1,250 @@
+# coding: utf-8
+
+from __future__ import print_function, division
+import search_engine.util as util
+import os
+import pandas
+import re
+import copy
+
+
+def extract_links(page_id, page_content, page_to_id):
+    """
+    Search for links in page content
+    :param page_id:
+    :param page_content:
+    :param page_to_id:
+    :return:
+    """
+    re_link = '\[\[(.*?)\]\]'
+    match = re.findall(re_link, page_content)
+    links = []
+    if match:
+        for m in match:
+            for m1 in m.split('|'):
+                if ':' in m1:
+                    continue
+                m1 = util.normalize_text(m1)
+                if m1 in page_to_id:
+                    links = links + page_to_id[m1]
+    return sorted(set(links) - set([page_id]))
+
+
+def extract_words_id(text, word_to_id):
+    """
+    Convert a text to an array of word ids
+    :param text:
+    :param word_to_id:
+    :return:
+    """
+    text = util.normalize_text(text)
+    return [word_to_id[w] for w in text.split(' ') if w in word_to_id]
+
+
+def append_to_words_appearance(words_appearance, page_id, title, words_id, word_to_id):
+    """
+    Append words frequences to the words_appearance map
+    The frequency of a word in the words_id is between 0 and 1
+    The words in the title add the value 1 to the frequency of the corresponding word
+    :param words_appearance:
+    :param page_id:
+    :param title:
+    :param words_id:
+    :param word_to_id:
+    :return:
+    """
+    title_words_id = set(word_to_id[w] for w in title.split(' ') if w in word_to_id)
+    freq_unique = 1.0 / float(len(words_id))
+    occ = dict()
+    for word_id in words_id:
+        if word_id in occ:
+            occ[word_id] += freq_unique
+        else:
+            occ[word_id] = freq_unique
+    for word_id in title_words_id:
+        if word_id in occ:
+            occ[word_id] += 1.0
+        else:
+            occ[word_id] = 1.0
+    for k, v in occ.items():
+        if k in words_appearance:
+            words_appearance[k].append((page_id, v))
+        else:
+            words_appearance[k] = [(page_id, v)]
+
+
+def run(wiki_dump_filename, output_dir, dictionary_filename, print_interval=10000):
+    if wiki_dump_filename is None:
+        return
+    if output_dir is None:
+        return
+
+    PAGE_TO_ID_FILENAME = os.path.join(output_dir, 'page_to_id.csv')
+    ID_TO_PAGE_FILENAME = os.path.join(output_dir, 'id_to_page.csv')
+    PAGE_LINKS_FILENAME = os.path.join(output_dir, 'page_links.csv')
+    WORDS_APPEARANCE_FILENAME = os.path.join(output_dir, 'words_appearance.csv')
+
+    print('Counting lines in wiki file')
+    WIKI_N_LINES = util.count_lines_in_file(wiki_dump_filename)
+    print('Result:', util.pretty_number(WIKI_N_LINES))
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+
+    print('\nLoading dictionnary of words ...')
+    dataframe = pandas.read_csv(dictionary_filename)
+    word_to_id = {}
+    id_to_word = {}
+    for i in range(len(dataframe)):
+        word_to_id[dataframe['word'][i]] = dataframe['id'][i]
+        id_to_word[dataframe['id'][i]] = dataframe['word'][i]
+    print('Dictionnary loaded')
+
+
+    page_to_id = {}  # page title => [ids]
+    id_to_page = {}  # id => page title
+
+    progressBar = util.ProgressBar(WIKI_N_LINES)
+    fd_wiki_dump = open(wiki_dump_filename, 'r')
+    re_title = '.*<title>(.+)</title>.*'
+    line_count = 0
+    page_id = 1
+
+    print('\nSearch for page titles in file')
+    for line in fd_wiki_dump:
+        line = line[:-1]
+        line = line.strip()
+        if '<title' in line:
+            line = util.process_word(line.decode('utf-8'))
+            match = re.match(re_title, line)
+            if match is None or ':' in match.group(1):
+                continue
+            current_title = match.group(1)
+            current_title = util.normalize_text(current_title)
+            if current_title in page_to_id:
+                page_to_id[current_title].append(page_id)
+            else:
+                page_to_id[current_title] = [page_id]
+            id_to_page[page_id] = current_title
+            page_id += 1
+        if line_count % print_interval == 0:
+            progressBar.print_progress(line_count)
+        line_count += 1
+
+    print(' DONE')
+    print(util.pretty_number(len(id_to_page)), 'titles found\n')
+
+
+    # sorting ids for each page titles
+    for k, v in page_to_id.items():
+        page_to_id[k] = sorted(v)
+
+
+    in_page = False
+    in_text = False
+    words = None
+    page_title = None
+
+    re_text_start = '^.*<text.*>(.+)$'
+    re_text_end = '^(.*)</text>'
+
+    page_to_id_cpy = copy.deepcopy(page_to_id)
+
+    words_appearance = dict()
+
+    fd_wiki_dump.seek(0,0)
+
+    fd_links = open(os.path.join(PAGE_LINKS_FILENAME), 'w')
+    fd_links.write('page_id,links\n')
+
+    line_count = 0
+    progressBar.reset_time()
+
+    print('Collecting text of each page in file')
+    for line in fd_wiki_dump:
+        line = line[:-1]
+        line = line.strip()
+        if in_page:
+            if in_text:
+                line = util.process_word(line.decode('utf-8'))
+                if '</text' in line:
+                    match = re.match(re_text_end, line)
+                    if match is None:
+                        continue
+                    words += ' ' + match.group(1)
+                    in_text = False
+                else:
+                    words += ' ' + line
+            else:
+                if '</page' in line:
+                    if page_title and words:
+                        page_id = page_to_id_cpy[page_title][0]
+                        page_to_id_cpy[page_title] = page_to_id_cpy[page_title][1:]
+
+                        links = extract_links(page_id, words, page_to_id)
+                        fd_links.write('{},{}\n'.format(page_id, ' '.join([str(k) for k in links])))
+
+                        words_id = extract_words_id(words, word_to_id)
+                        if len(words_id) > 0:
+                            append_to_words_appearance(
+                                words_appearance,
+                                page_id,
+                                page_title,
+                                words_id,
+                                word_to_id
+                            )
+                    words = None
+                    page_title = None
+                    in_page = False
+                elif '<title' in line:
+                    line = util.process_word(line.decode('utf-8'))
+                    match = re.match(re_title, line)
+                    if match is None:
+                        continue
+                    page_title = match.group(1)
+                    page_title = util.normalize_text(page_title)
+                elif '<text' in line:
+                    line = util.process_word(line.decode('utf-8'))
+                    match = re.match(re_text_start, line)
+                    if match is None:
+                        continue
+                    words = match.group(1)
+                    in_text = True
+        else:
+            if '<page' in line:
+                in_page = True
+        if line_count % print_interval == 0:
+            progressBar.print_progress(line_count)
+        line_count += 1
+
+    fd_links.close()
+    fd_wiki_dump.close()
+    print('  DONE\n')
+    print(PAGE_LINKS_FILENAME, 'contain page links')
+
+    print('Sorting page to id array')
+    page_to_id = sorted(page_to_id.items())
+    print('Sorting id to page array')
+    id_to_page = sorted(id_to_page.items())
+
+    print('Sorting words frequencies array')
+    words_appearance = sorted(words_appearance.items())
+
+    print('Saving page to id array to', PAGE_TO_ID_FILENAME)
+    with open(PAGE_TO_ID_FILENAME, 'w') as fd:
+        fd.write('page@ids\n')
+        for page, list_id in page_to_id:
+            fd.write('{}@{}\n'.format(page, ' '.join([str(k) for k in list_id])))
+
+    print('Saving id to page array to', ID_TO_PAGE_FILENAME)
+    with open(ID_TO_PAGE_FILENAME, 'w') as fd:
+        fd.write('id@page\n')
+        for page_id, page in id_to_page:
+            fd.write('{}@{}\n'.format(page_id, page))
+
+    print('Saving words frequencies array to', WORDS_APPEARANCE_FILENAME)
+    with open(WORDS_APPEARANCE_FILENAME, 'w') as fd:
+        fd.write('word_id,frequencies\n')
+        for word_id, freqs in words_appearance:
+            fd.write('{},{}\n'.format(word_id, ' '.join([str(a) + ':' + str(b) for a, b in freqs])))
